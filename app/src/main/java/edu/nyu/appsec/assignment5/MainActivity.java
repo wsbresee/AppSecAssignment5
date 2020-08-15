aa68a24498bd8ee52d2358c1fdf21b8902d32c0b
Willy <wsbresee@gmail.com>
2020-08-15 15:22:47 -0400

    added everything

---
+++ app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
@@ -0,0 +1,46 @@
+package edu.nyu.appsec.assignment5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String SPELL_CHECK_URL = "http://appsecclass.report:8080/";
+    private static final String KNOWN_HOST = "appsecclass.report";
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = String.valueOf(request.getUrl());
+            String host = Uri.parse(url).getHost();
+
+            if (KNOWN_HOST.equals(host)) {
+                return false;
+            }
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = new WebView(this);
+        view.setWebViewClient(new MyWebViewClient());
+
+        WebSettings settings = view.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        setContentView(view);
+        view.loadUrl(SPELL_CHECK_URL + "register");
+    }
+}
